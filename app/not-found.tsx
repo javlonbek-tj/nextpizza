6a72919f8a04f8e62fa59f14a51c986016f47153
javlonbek-tj <tjmr1996@gmail.com>
2025-08-26 18:24:44 +0500

    initial commit

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Page Not Found',
+};
+const NotFoundPage = () => {
+  return (
+    <div className='text-2xl flex justify-center items-center min-h-screen'>
+      Page not Found
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = 'NEXT PIZZA';
+export const APP_DESCRIPTION = 'Best pizza in the universe';
